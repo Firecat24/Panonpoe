5ab4c84abaf3324c29a86e408c0262ec20358dca
Firecat24 <Farhancrims1@gmail.com>
2025-08-21 15:13:56 +0700

    penghapusan perhitunggan

--- PERHITUNGAN/app/static/javascript/daftar_login.js
+++
@@ -1,8 +0,0 @@
-setTimeout(function() {
-    var alertElements = document.querySelectorAll('.alert');
-    alertElements.forEach(function(alertElement) {
-        alertElement.classList.remove('show');
-        alertElement.classList.add('d-none');
-        setTimeout(function() { alertElement.remove(); }, 1000);
-    });
-}, 5000);
