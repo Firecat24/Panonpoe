a094435e9ba1ab37a23f37ab6519e020c728efd3
Firecat24 <Farhancrims1@gmail.com>
2025-09-10 13:26:21 +0700

    penyesuaian

--- PUBLISHER/app/config.py
+++ PUBLISHER/app/config.py
@@ -4,11 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")  # untuk session/flash
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_USER = os.getenv("DB_USER", "root")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "")
-    DB_NAME = os.getenv("DB_NAME", "publisher")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_NAME = os.getenv("DB_NAME")
 
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     UPLOAD_MANUSCRIPTS = os.path.join(BASE_DIR, "app", "uploads", "manuscripts")
