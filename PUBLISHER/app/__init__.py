5e1c41cd37bba84108af74eceb4dca0589b015e5
Firecat24 <Farhancrims1@gmail.com>
2025-09-13 16:09:55 +0700

    Perbaruan kecil

--- PUBLISHER/app/__init__.py
+++ PUBLISHER/app/__init__.py
@@ -3,15 +3,15 @@ from .db.db import Database
 
 app = Flask(__name__)
 
-@app.before_request
-def before_request():
-    g.db = Database()
-    g.db.connect()
+# @app.before_request
+# def before_request():
+#     g.db = Database()
+#     g.db.connect()
 
-@app.teardown_request
-def teardown_request(exception):
-    if hasattr(g, "db"):
-        g.db.disconnect()
+# @app.teardown_request
+# def teardown_request(exception):
+#     if hasattr(g, "db"):
+#         g.db.disconnect()
 
 from .routes.routes_submit import bp_submit
 from .routes.routes_dashboard import bp_dashboard
