b5511bf9be803dde372292e9a3a9452d3accc617
Firecat24 <Farhancrims1@gmail.com>
2025-09-30 16:10:29 +0700

    perbaikan 1

--- PRODUK/app/__init__.py
+++ PRODUK/app/__init__.py
@@ -15,9 +15,9 @@ app = Flask(__name__)
 
 from .routes.routes_dashboard import bp_dashboard
 from .routes.routes_akrilikdome import bp_akrilikdome
-from .routes.routes_siqilat import bp_siqiblat
+from .routes.routes_siqiblat import bp_siqiblat
 from .routes.routes_teleskop import bp_teleskop
 app.register_blueprint(bp_dashboard)
 app.register_blueprint(bp_akrilikdome, url_prefix="/akrilikdome")
-app.register_blueprint(bp_siqiblat, url_prefix="/siqilat")
+app.register_blueprint(bp_siqiblat, url_prefix="/siqiblat")
 app.register_blueprint(bp_teleskop, url_prefix="/teleskop")
