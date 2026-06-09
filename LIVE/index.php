db56ac22bee3c7febc28ae6b016d61d3fb1419b5
Firecat24 <Farhancrims1@gmail.com>
2026-06-09 14:54:01 +0700

    penambahan sedikit utuk cache manage

--- LIVE/index.php
+++ LIVE/index.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="Cache-Control" content="no-cache, must-revalidate">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
         <link rel="stylesheet" href="css/plugins/magnific-popup.css">
